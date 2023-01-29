a8d1cae5db6f839ba9a7eb6c65bc8deb395120b1
sankaire <kerenketepela@gmail.com>
2023-01-29 14:33:50 +0300

    Feat:export docs route

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,4 +1,5 @@
 import user_router from "./auth/users";
+import docs_router from "./docs/docs";
 import admin_router from "./user/index.routes";
 
-export default { user_router, admin_router };
+export default { user_router, admin_router, docs_router };
